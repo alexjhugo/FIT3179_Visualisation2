de155251772e72369759e51b7d065a9ce35119ca
alexjhugo <alxndrhugo@gmail.com>
2022-10-09 20:22:31 +1100

    major changes

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,5 +1,5 @@
 var vg_1 = "tokyo2020.vg.json";
 vegaEmbed("#map", vg_1).then(function(result) {}).catch(console.error);
 
-var vg_2 = "isotype.vg.json";
-vegaEmbed("#isotype", vg_2).then(function(result) {}).catch(console.error);
+var vg_2 = "medal_heatmap.vg.json";
+vegaEmbed("#heatmap", vg_2).then(function(result) {}).catch(console.error);
