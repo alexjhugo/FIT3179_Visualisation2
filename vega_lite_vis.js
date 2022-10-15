f377dcf5495c6b04a1160a0fc47b2598ebc63676
alexjhugo <alxndrhugo@gmail.com>
2022-10-15 15:15:14 +1100

    new stuff

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,8 +1,8 @@
-var vg_1 = "tokyo2020.vg.json";
+var vg_1 = "vega/medal_globe.vg.json";
 vegaEmbed("#map", vg_1).then(function(result) {}).catch(console.error);
 
-var vg_2 = "medal_heatmap.vg.json";
+var vg_2 = "vega/medal_heatmap.vg.json";
 vegaEmbed("#heatmap", vg_2).then(function(result) {}).catch(console.error);
 
-var vg_3 = "gender_bar.vg.json";
+var vg_3 = "vega/gender_bar.vg.json";
 vegaEmbed("#groupedbar", vg_3).then(function(result) {}).catch(console.error);
