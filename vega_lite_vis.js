998dae579104a964f05b44c60aaebbd181fd80c9
alexjhugo <alxndrhugo@gmail.com>
2022-10-16 15:10:16 +1100

    major changes

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,8 +1,10 @@
-var vg_1 = "vega/medal_globe.vg.json";
+var vg_1 = "vega/globe.vg.json";
 vegaEmbed("#map", vg_1).then(function(result) {}).catch(console.error);
 
-var vg_2 = "vega/medal_heatmap.vg.json";
+var vg_2 = "vega/heatmap.vg.json";
 vegaEmbed("#heatmap", vg_2).then(function(result) {}).catch(console.error);
 
-var vg_3 = "vega/gender_bar.vg.json";
+var vg_3 = "vega/groupedBar.vg.json";
 vegaEmbed("#groupedbar", vg_3).then(function(result) {}).catch(console.error);
+
+var vg_4 = "vega/choropleth.vg.json";
