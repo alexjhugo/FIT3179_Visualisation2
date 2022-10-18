d3328749c420ed805932678e0c5c30d235c7d69d
alexjhugo <alxndrhugo@gmail.com>
2022-10-18 11:14:26 +1100

    latest

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -5,7 +5,7 @@ var vg_2 = "vega/heatmap.vg.json";
 vegaEmbed("#heatmap", vg_2, {"actions": false}).then(function(result) {}).catch(console.error);
 
 var vg_3 = "vega/groupedBar.vg.json";
-vegaEmbed("#groupedbar", vg_3).then(function(result) {}).catch(console.error);
+vegaEmbed("#groupedbar", vg_3, {"actions": false}).then(function(result) {}).catch(console.error);
 
 var vg_4 = "vega/chorpleth.vg.json";
 vegaEmbed("#choropleth", vg_4, {"actions": false}).then(function(result) {}).catch(console.error);
